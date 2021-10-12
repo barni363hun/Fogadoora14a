963ead12903a31ebfdd5968fc1213730674189d0
CserpakRobert <robi.cserpak@gmail.com>
2021-10-12 09:22:32 +0200

    Megoldas class constructorának létrehozása

--- src/megoldas.ts
+++ src/megoldas.ts
@@ -1,8 +1,13 @@
+const fs = require("fs");
+import { readFileSync } from "fs";
 import Foglalas from "./foglalas";
 
 export default class Megoldas {
     foglalasok: Foglalas[] = [];
     constructor(fajlNev: string) {
-        
+        readFileSync(fajlNev)
+            .toString()
+            .split("\n\r")
+            .forEach(f => this.foglalasok.push(new Foglalas(f)));
     }
 }
