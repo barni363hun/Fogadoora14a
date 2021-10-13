ec6734eb81e7a1c0b96efd9230e314e738e4a193
CserpakRobert <robi.cserpak@gmail.com>
2021-10-13 13:50:36 +0200

    Content file osztály kitakarítása és Content2 file törlése

--- src/Content.ts
+++ src/Content.ts
@@ -11,34 +11,6 @@ export default class Content {
             return;
         }
         // Weboldal inicializálása + head rész:
-        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-        res.write("<!DOCTYPE html>");
-        res.write("<html lang='hu'>");
-        res.write("<head>");
-        res.write("<meta charset='utf-8'>");
-        res.write("<style>input, pre {font-family:monospace; font-size:1em; font-weight:bold;}</style>");
-        res.write("<meta name='viewport' content='width=device-width, initial-scale=1.0'>");
-        res.write("<title>Jedlik Ts Template</title>");
-        res.write("</head>");
-        res.write("<body><form><pre>");
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const params = new url.URL(req.url as string, `http://${req.headers.host}/`).searchParams;
-
-        // Kezd a kódolást innen -->
-
-        res.write("Egyszerű Hello World! (2021/2022)\n");
-
-        // Tetszőleges html teg-ek és attribútumok beépítése:
-        res.write("<span style='color: blue;'><i>Színes és dőlt Hello World!'</i></span>\n");
-
-        // Próbáljuk számra konvertálni a "kor" paraméter (http://localhost:8080/?kor=16) értékét:
-        let korod = parseInt(params.get("kor") as string);
-        // Ha nincs "kor" paraméter megadva, vagy nem lehet számra konvertálni értékét,
-        // akkor a "korod" változóba NaN érték kerül, ilyenkor legyen 18 év az értéke:
-        if (isNaN(korod)) korod = 18;
-
-        res.write(`<label>Kérem a korod: <input type='number' name='kor' value=${korod} style='max-width:100px;' onChange='this.form.submit();'></label>\n`);
-        res.write(`Te ${korod} éves vagy!\n`);
 
         // <---- Fejezd be a kódolást
 
