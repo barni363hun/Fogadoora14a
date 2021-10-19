ca04879e114718d36865ed999a56b6bc1492a957
CserpakRobert <robi.cserpak@gmail.com>
2021-10-19 08:50:32 +0200

    idopontKiirasaFajlba() teszteléseének megírása

--- src/__tests__/megoldas.test.ts
+++ src/__tests__/megoldas.test.ts
@@ -3,7 +3,7 @@ import Foglalas from "../foglalas"
 import { readFileSync } from "fs";
 
 const megoldas1: Megoldas = new Megoldas("src/__tests__/tesztFajl.txt")
-test("konstruktor tesztelése fájlból", () => {
+test("konstruktor tesztelése fájlból", async () => {
     expect(megoldas1).toBeInstanceOf(Megoldas);
     expect(megoldas1.foglalasok).toBeInstanceOf(Array)
     expect(megoldas1.foglalasok[2]).toBeInstanceOf(Foglalas)
@@ -11,4 +11,8 @@ test("konstruktor tesztelése fájlból", () => {
     expect(() => new Megoldas("nemLetezoFajl.txt")).toThrow()
 
 
+})
+test("idopontKiirasaFajlba() tesztelése", async () => {
+    expect(megoldas1.IdopontKiirasaFajlba("16:30")).toBe("Barna Eszter<br>Csorba Ede<br>Magos Magdolna<br>Neumann Nikolett<br>");
+    expect(readFileSync("fajlok/1630.txt").toString()).toBe("Barna Eszter\r\nCsorba Ede\r\nMagos Magdolna\r\nNeumann Nikolett");
 })
