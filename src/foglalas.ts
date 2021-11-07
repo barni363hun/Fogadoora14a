99bf0e0f9ce137e89fd85436fcd66e42c625fa5e
nitslaszlo <nitslaszlo@gmail.com>
2021-11-07 17:05:30 +0100

    észrevétel

--- src/foglalas.ts
+++ src/foglalas.ts
@@ -1,12 +1,19 @@
+// eslint formázási és típuseltérési hibákat jelez, biztosan nem futott az utolsó modosításnál (bemutató)
+// PDF állományok nevei
+// induláskor nincsenek alapértelmetett értékek
+// nincs Heroku link
+// teszt elhasal
+// nem működik
+
 export default class Foglalas {
     #vezetekNev: string;
     #keresztNev: string;
     #lefoglaltIdopont: string;
     #foglalasIdeje: Date;
-    #foglalasStringben :string;
-    
+    #foglalasStringben: string;
+
     constructor(sor: string) {
-        let adatok: string[] = sor.split(" ");
+        const adatok: string[] = sor.split(" ");
         this.#vezetekNev = adatok[0];
         this.#keresztNev = adatok[1];
         this.#lefoglaltIdopont = adatok[2];
@@ -14,29 +21,28 @@ export default class Foglalas {
         this.#foglalasIdeje = new Date(Date.parse(adatok[3]));
     }
 
-    idoponthozTartozoNev(keresettIdopont: string): (string | null) {
-        return keresettIdopont == this.#lefoglaltIdopont ? (this.teljesNev) : null;
+    idoponthozTartozoNev(keresettIdopont: string): string | null {
+        return keresettIdopont == this.#lefoglaltIdopont ? this.teljesNev : null;
     }
-  
-    nevheztartozoIdopont(nev: string): (string | null) {
-        if (nev == this.teljesNev)
-            return this.#lefoglaltIdopont;
+
+    nevheztartozoIdopont(nev: string): string | null {
+        if (nev == this.teljesNev) return this.#lefoglaltIdopont;
         return null;
     }
-  
+
     public get teljesNev(): string {
         return this.#vezetekNev + " " + this.#keresztNev;
     }
 
-    public get foglalasDate():Date{
+    public get foglalasDate(): Date {
         return this.#foglalasIdeje;
     }
-  
-    public get idopont():string{
+
+    public get idopont(): string {
         return this.#lefoglaltIdopont;
     }
-  
-    public get foglalasString():string{
+
+    public get foglalasString(): string {
         return this.#foglalasStringben;
     }
 }
